739926f21b21c8a8023a6a8da1adac0acdaceaa4
Ryan Svihla <rssvihla@gmail.com>
2010-01-29 11:23:24 -0600

    changed api auth url https://auth.api.rackspacecloud.com/v1.0

--- com.mosso.cloudfiles.integration.tests/Constants.cs
+++ com.mosso.cloudfiles.integration.tests/Constants.cs
@@ -17,7 +17,7 @@ namespace com.mosso.cloudfiles.integration.tests
         public const string CONTAINER_NAME = "integration.tests.container.new";
          
         //Mosso accounts for authentication
-        public const string MOSSO_AUTH_URL = "https://api.mosso.com/auth";
+        public const string MOSSO_AUTH_URL = "https://auth.api.rackspacecloud.com/v1.0";
 
         //Response constants
         public const int NoContentResponse = 204;
