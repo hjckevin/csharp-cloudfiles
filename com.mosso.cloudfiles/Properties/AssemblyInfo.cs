bda56f529b710243602c6ffc3e798c11fbd65069
Ryan Svihla <rssvihla@gmail.com>
2010-01-30 07:23:17 -0600

    fixed typo in rake file that broke by hand builds

--- com.mosso.cloudfiles/Properties/AssemblyInfo.cs
+++ com.mosso.cloudfiles/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 using System.Reflection;
 using System.Security;
 
-[assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyVersion("1.4.1.3")]
-[assembly: AssemblyInformationalVersion("1.4.1.3")]
+[assembly: AssemblyVersion("1.4.2.1")]
 [assembly: AssemblyDescription("C#.NET API for Rackspace Cloud Files Cloud Storage")]
-[assembly: AssemblyCopyright("Copyright (c) 2008 2009, Rackspace Managed Hosting.  All Rights Reserved")]
 [assembly: AssemblyCompany("Rackspace Managed Hosting")]
+[assembly: AssemblyInformationalVersion("1.4.2.1")]
 [assembly: AssemblyProduct("csharp-floudfiles")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyCopyright("Copyright (c) 2008 2009, Rackspace Managed Hosting.  All Rights Reserved")]
 		
