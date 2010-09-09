2ae60dc9ae7a6ab097ce757e00fba93f531e7916
Jason Meridth <jmeridth@gmail.com>
2010-09-08 21:07:47 -0500

    _actually_ update version to 1.4.2.7 and update README

--- com.mosso.cloudfiles/Properties/AssemblyInfo.cs
+++ com.mosso.cloudfiles/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 using System.Reflection;
 using System.Security;
 
-[assembly: AssemblyCompany("Rackspace Managed Hosting")]
-[assembly: AssemblyVersion("1.4.2.5")]
 [assembly: AssemblyProduct("csharp-floudfiles")]
-[assembly: AssemblyInformationalVersion("1.4.2.5")]
 [assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyVersion("1.4.2.7")]
 [assembly: AssemblyDescription("C#.NET API for Rackspace Cloud Files Cloud Storage")]
 [assembly: AssemblyCopyright("Copyright (c) 2008 2009 2010, Rackspace Managed Hosting.  All Rights Reserved")]
+[assembly: AssemblyInformationalVersion("1.4.2.7")]
+[assembly: AssemblyCompany("Rackspace Managed Hosting")]
 		
