b76905ef105d5ae37b67fa2f8a9668772d66244e
Jason Seney <jrseney@gmail.com>
2010-12-02 15:02:46 -0500

    - Adding documentation to AccountInformation.cs

--- com.mosso.cloudfiles/Domain/AccountInformation.cs
+++ com.mosso.cloudfiles/Domain/AccountInformation.cs
@@ -1,40 +1,44 @@
-///
-/// See COPYING file for licensing information
-///
-
-using System;
+//----------------------------------------------
+// See COPYING file for licensing information
+//----------------------------------------------
 
 namespace com.mosso.cloudfiles.domain
 {
+    #region Using
+    using System;
+    #endregion
+
     /// <summary>
-    /// encapsulates the container count and bytes used by a customer
+    /// Encapsulates customer's account information including the container count and bytes used.
     /// </summary>
     public class AccountInformation
     {
         /// <summary>
-        /// the account information of a customer
+        /// Initializes a new instance of AccountInformation class for customer
         /// </summary>
-        /// <param name="containerCount">the number of containers a customer owns</param>
-        /// <param name="bytesUsed">the bytes used by a customer</param>
+        /// <param name="containerCount">The number of containers a customer owns</param>
+        /// <param name="bytesUsed">The bytes used by a customer</param>
         /// <exception cref="System.ArgumentNullException">Thrown when any of the reference arguments are null</exception>
         public AccountInformation(string containerCount, string bytesUsed)
         {
             if (string.IsNullOrEmpty(containerCount) ||
                 string.IsNullOrEmpty(bytesUsed))
+            {
                 throw new ArgumentNullException();
+            }
 
-            ContainerCount = int.Parse(containerCount);
-            BytesUsed = long.Parse(bytesUsed);
+            this.ContainerCount = int.Parse(containerCount);
+            this.BytesUsed = long.Parse(bytesUsed);
         }
 
         /// <summary>
-        /// the number of the containers a customer owns
+        /// Gets or sets the number of the containers a customer owns
         /// </summary>
         /// <return>number of containers</return>
         public int ContainerCount { get; set; }
 
         /// <summary>
-        /// the bytes used by a customer
+        /// Gets or sets the bytes used by a customer
         /// </summary>
         /// <return>number of bytes</return>
         public long BytesUsed { get; set; }
