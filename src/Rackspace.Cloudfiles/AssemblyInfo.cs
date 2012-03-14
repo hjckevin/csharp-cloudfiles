75d7acdfb48481b2922e72c9e3b522eafd7b0887
jmeridth <jmeridth@gmail.com>
2012-03-14 13:43:52 -0500

    update nuspec and assemblyinfo versions

--- src/Rackspace.Cloudfiles/AssemblyInfo.cs
+++ src/Rackspace.Cloudfiles/AssemblyInfo.cs
@@ -1,27 +1,8 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Rackspace.Cloudfiles")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("cweidenkeller")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("3.0.2.4")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyDescription("C#.NET API for Rackspace Cloud Files Cloud Storage")]
+[assembly: AssemblyCompany("Rackspace Managed Hosting")]
+[assembly: AssemblyProduct("csharp-cloudfiles")]
+[assembly: AssemblyCopyright("Copyright (c) 2008, 2009 2010, 2011, 2012, Rackspace Managed Hosting.  All Rights Reserved")]
+[assembly: AssemblyVersion("3.0.2.5")]
