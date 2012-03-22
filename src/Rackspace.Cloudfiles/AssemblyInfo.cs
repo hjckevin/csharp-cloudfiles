3e9a02908b6b5537b7f487d4bc7cc3f6de81151c
jmeridth <jmeridth@gmail.com>
2012-03-22 18:12:33 -0500

    upgrade csharp-swift to 1.0.1 and update assemblyinfo and nuspec version

--- src/Rackspace.Cloudfiles/AssemblyInfo.cs
+++ src/Rackspace.Cloudfiles/AssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("Rackspace Managed Hosting")]
 [assembly: AssemblyProduct("csharp-cloudfiles")]
 [assembly: AssemblyCopyright("Copyright (c) 2008, 2009 2010, 2011, 2012, Rackspace Managed Hosting.  All Rights Reserved")]
-[assembly: AssemblyVersion("3.0.2.6")]
+[assembly: AssemblyVersion("3.0.2.7")]
